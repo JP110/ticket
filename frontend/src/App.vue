9cbb0e860630ed27a7d112c9edc4e19d8db4ce4e
Jerome Puch <108734960+Stealer110@users.noreply.github.com>
2024-01-10 11:21:45 +0100

    form update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,49 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import {ref} from 'vue'
+const formData = ref ( {
+  email: '',
+  category: '',
+  description: '',
+  priority: '',
+})
+
+// handle sign out
+const submitForm = function (e) { console.log(formData.value.email)
+// submit sign out here
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <form class="w-fit m-auto " @submit.prevent="submitForm">
+    <div class="flex justify-between mb-4">
+      <label class="w-4/12"  for="email">Email</label>
+      <input class="border w-8/12" name = "email"  v-model="formData.email">
+    </div>
+    
+    <div class="flex justify-between mb-4">
+      <label class="w-4/12" for="category">Category</label>
+      <select class="w-8/12" name="category" v-model="formData.category">
+        <option disabled value="">Choisissez</option>
+        <option>Mobilier</option>
+        <option>Informatique</option>
+        <option>Autre</option>
+    </select>
+    </div>
+    <div class="flex justify-between mb-4">
+      <label class="w-4/12" for="description">Description</label>
+      <input class="border w-8/12" name="description"  v-model="formData.description">
+    </div>
+    <div class="flex justify-between mb-4">
+      <label class="w-4/12" for="urgence">Priority</label>
+      <select class="w-8/12" name="urgence" v-model="formData.priority">
+        <option disabled value="">Choisissez</option>
+        <option>Faible</option>
+        <option>Normale</option>
+        <option>Haute</option>
+    </select>
+    </div>
+   <button  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Valider</button>
+</form>
+  
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
